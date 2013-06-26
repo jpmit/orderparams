8e65995859a6588135ec83929e094fd09c09b717
James Mithen <j.mithen@surrey.ac.uk>
2013-06-26 17:45:44 +0100

    added header for specifying compilation options

---
+++ src/compile.h
@@ -0,0 +1,12 @@
+#ifndef COMPILE_H
+#define COMPILE_H
+
+// enable logging (prints diagnostic information when running program)
+
+const bool LOGGING = false;
+
+// string printed to output before log message
+
+const char LOGMSG[] = "LOG: ";
+
+#endif
